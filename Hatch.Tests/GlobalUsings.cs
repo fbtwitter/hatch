41796441024a52ab57b5a19273a5ba3ba908dfd5
Reza Fauzi Augusdi <rzaugusdi354@gmail.com>
2026-05-22 15:45:35 +0700

    test: add FlaUI tests for details pane

---
+++ Hatch.Tests/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using FlaUI.Core;
+global using FlaUI.Core.AutomationElements;
+global using FlaUI.Core.Conditions;
+global using FlaUI.UIA3;
